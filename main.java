18b8dbfb79fbbcb8e48a30ad379346df6c73ce48
punpunbaba <punbababa@gmail.com>
2024-01-21 14:04:47 +0700

    add

---
+++ main.java
@@ -0,0 +1,9 @@
+ 
+let menu = document.querySelector('#menu-icon');
+let sidenavbar = document.querySelector('.side-navbar');
+let content = document.querySelector('.content');
+ 
+menu.onclick = () => {
+    sidenavbar.classList.toggle('active');
+    content.classList.toggle('active');
+}
